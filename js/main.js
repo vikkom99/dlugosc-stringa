c60133a55cf5d0d52df3c548e558acd2147e6df3
vikkom99 <wikkomarik@gmail.com>
2022-06-12 22:49:43 +0100

    Poprawione

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
-let me = "Uwielbiam JavaScript";
-let js = "Jestem świetnym programistą";
-function num1 (me, js){
-    let result = me >= js;
-    return result
+let str1 = "Uwielbiam JavaScript";
+let str2 = "Jestem świetnym programistą";
+function num1(a, b) {
+    let result = Math.max(a.length, b.length);
+    if (a.length > b.length) {
+        console.log(str1 + ">" + str2)
+    } else {
+        console.log(str1 + " < " + str2)
+    }
+    return result;
 }
-console.log( num1() )
+console.log(num1(str1, str2))
